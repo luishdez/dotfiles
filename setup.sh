300c703e17140a1e7543ac04039186480f16665e
luishdez <luis.munoz.hdez@gmail.com>
2025-08-06 00:45:40 +0200

    fix(brew): Remove --adopt flag from env stow command

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 cd "$(dirname "$0")"
 
 echo "Stowing all packages..."
-stow --adopt -v -t $HOME env
+stow -v -t $HOME env
 stow -v -t $HOME ssh
 stow -v -t $HOME zsh
 stow -v -t $HOME git
